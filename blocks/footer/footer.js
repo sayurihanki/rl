cac1b86c92f3950d321682bfdc8cd3adc509808a
Lamont Crook <lamont@adobe.com>
2023-03-12 15:26:03 -0600

    update

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,4 +1,5 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { createTag } from '../../scripts/scripts.js';
 
 /**
  * loads and decorates the footer
@@ -12,8 +13,21 @@ export default async function decorate(block) {
   const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
   const html = await resp.text();
+
   const footer = document.createElement('div');
   footer.innerHTML = html;
-  await decorateIcons(footer);
+  const h3s = footer.querySelectorAll('h3');
+
+  h3s.forEach((h3) => {
+    const div = createTag('div', { class: 'footer-element'});
+    const ul = h3.nextElementSibling;
+    div.appendChild(h3);
+    div.appendChild(ul);
+    footer.appendChild(div);
+  });
+
+  footer.querySelector('div').remove();
+
+  await decorateIcons(footer);  
   block.append(footer);
 }
